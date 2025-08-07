1ca8f7984adb566828693af6571465352b7ded7e
HannahsCatCafe <hannah.str01@gmail.com>
2025-08-07 20:32:43 +0200

    Fix deploy error: remove abort-controller import (use built-in AbortController)

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const dotenv = require('dotenv');
 const path = require('path');
-const { AbortController } = require('abort-controller');
 
 dotenv.config();
 const app = express();
